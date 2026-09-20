46dbdaca8e7eda2946bc71eec338ad947b572ee8
fernandoluca015 <fernandooluca133@gmail.com>
2026-09-20 18:13:19 -0300

    Refactor tests for statistics functions

--- tests/test_pystatistics.py
+++ tests/test_pystatistics.py
@@ -1,70 +1,59 @@
 import math
 import numpy as np
-<<<<<<< HEAD
-from scipy import stats
-
 
 from src.core.pystatistics import (
-    media, 
-    desvio_padrao, 
-    correlacao_pearson, 
-    pdf_normal
-=======
-
-from src.minhastats import (
     media,
     mediana,
     variancia,
     desvio_padrao,
-    correlacao_pearson,
->>>>>>> upstream/development
+    correlacao_pearson
 )
+
 
 dados = [10, 12, 23, 23, 16, 23, 21, 16]
 
-<<<<<<< HEAD
-def test_media():
-    assert math.isclose(media(dados), np.mean(dados), rel_tol=1e-5)
-
-def test_desvio_padrao():
-    assert math.isclose(desvio_padrao(dados, amostral=True), np.std(dados, ddof=1), rel_tol=1e-5)
-=======
 
 def test_media():
-    assert math.isclose(media(dados), np.mean(dados), rel_tol=1e-5)
+    assert math.isclose(
+        media(dados),
+        np.mean(dados),
+        rel_tol=1e-5
+    )
+
+
+def test_mediana():
+    assert math.isclose(
+        mediana(dados),
+        np.median(dados),
+        rel_tol=1e-5
+    )
+
+
+def test_variancia():
+    assert math.isclose(
+        variancia(dados, amostral=True),
+        np.var(dados, ddof=1),
+        rel_tol=1e-5
+    )
 
 
 def test_desvio_padrao():
     assert math.isclose(
         desvio_padrao(dados, amostral=True),
         np.std(dados, ddof=1),
-        rel_tol=1e-5,
+        rel_tol=1e-5
     )
 
->>>>>>> upstream/development
 
 def test_correlacao():
     x = [1, 2, 3, 4, 5]
     y = [2, 4, 5, 4, 5]
-<<<<<<< HEAD
-    r_proprio = correlacao_pearson(x, y)
-    r_np = np.corrcoef(x, y)[0, 1]
-    assert math.isclose(r_proprio, r_np, rel_tol=1e-5)
-
-
-def test_pdf_normal():
-    x_teste = 10.0
-    media_teste = 12.0
-    desvio_teste = 2.5
-    
-    val_nosso = pdf_normal(x_teste, media_teste, desvio_teste)
-    val_oficial = stats.norm.pdf(x_teste, loc=media_teste, scale=desvio_teste)
-    
-    assert math.isclose(val_nosso, val_oficial, rel_tol=1e-5)
-=======
 
     r_proprio = correlacao_pearson(x, y)
     r_np = np.corrcoef(x, y)[0, 1]
 
-    assert math.isclose(r_proprio, r_np, rel_tol=1e-5)
->>>>>>> upstream/development
+    assert math.isclose(
+        r_proprio,
+        r_np,
+        rel_tol=1e-5
+    )
